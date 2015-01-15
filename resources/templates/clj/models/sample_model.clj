e149b200abd320b0677567b4471a077c092f2e44
Truong <me@truongtx.me>
2015-01-15 09:29:38 +0700

    basic structure for model generating

--- resources/templates/clj/models/sample_model.clj
+++ resources/templates/clj/models/sample_model.clj
@@ -1,2 +1,48 @@
 (ns app.models.{{name}}
     (:use [korma.core]))
+
+(defentity users
+  ;; Basic configuration
+
+  ;; Table, by default the name of the entity
+  {% if table %}
+  (table :{{table}})
+  {% else %}
+  (table :{{name}})
+  {% endif %}
+
+  ;; by default "id". This line is unnecessary.
+  ;; it's used for relationships joins.
+  (pk :id)
+  (database db) ;; if none is specified the last defdb
+  ;; will be used. Also unnecessary.
+  (entity-fields :first :last) ;; default fields for selects
+
+  ;; Mutations
+  (prepare (fn [{last :last :as v}]
+             (if last
+               (assoc v :last (str/upper-case last)) v)))
+  ;; apply a function before storing in the db
+  ;; in this case the function changes the "last" field
+  ;; to upper case.
+  (transform (fn [{first :first :as v}]
+               (if first
+                 (assoc v :first (str/capitalize first)) v)))
+  ;; apply a function to all select results
+  ;; in this case the function changes the "first" field
+  ;; to capitalized.
+
+  ;; Relationships
+  (has-one address)
+  ;; assumes users.id = address.users_id
+  (has-many email)
+  ;; assumes users.id = email.users_id
+  ;; but gets the results in a second query
+  ;; for each element
+  (belongs-to account)
+  ;; assumes users.account_id = account.id
+  (many-to-many posts :users_posts))
+;; assumes a table users_posts with columns users_id
+;; and posts_id
+;; like has-many, also gets the results in a second
+;; query for each element
--- src/core/modelgen.clj
+++ src/core/modelgen.clj
@@ -1,12 +1,33 @@
 (ns core.modelgen
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [selmer.parser :as parser]))
+            [selmer.parser :as parser]
+            [clojure.string :as str]))
 
 (def cli-options
-  [["-n" "--name NAME" "Model Name"]])
+  [["-n" "--name NAME" "Model Name"]
+   ["-t" "--table TABLE" "Table Name"]])
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn validate [options errors]
+  (cond
+   errors (exit 1 (str/join "\n" errors)),
+   (not (:name options)) (exit 1 "Please input model name"),
+   :else true))
+
+(defn generate-model [options]
+  (println (parser/render-file "templates/clj/models/sample_model.clj" options))
+  )
 
 (defn -main
   [& args]
-  (def opts (parse-opts args cli-options))
-  (println opts)
-  (println (parser/render-file "templates/clj/models/sample_model.clj" {:name "hello"})))
+  (let [opts (parse-opts args cli-options)
+        options (:options opts)
+        errors (:errors opts)]
+
+    (println opts)
+
+    (validate options errors)
+    (generate-model options)))
