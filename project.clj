14b57b6c35b74f373b7416a6aa4886cd174419ac
Truong <me@truongtx.me>
2015-01-15 08:44:31 +0700

    add log4j

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                  [korma "0.4.0"]
                  [org.postgresql/postgresql "9.3-1100-jdbc41"]
                  [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [log4j "1.2.15" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
